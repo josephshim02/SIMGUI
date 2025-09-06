2c01bdb025f3d53f88e8f4d0ed941076544685f3
jackshee <s224003043@deakin.edu.au>
2025-09-07 02:33:46 +1000

    no CORS error, reintegrate bondgraphs time

--- backend/app.jl
+++ backend/app.jl
@@ -22,8 +22,8 @@ end
 # Echo endpoint for testing
 route("/echo", method = POST) do
   @show jsonpayload()
-
-  (jsonpayload()) |> json
+  
+  """{"drawflow":{"Home":{"data":{"1":{"id":1,"name":"f_store","data":{},"class":"f_store","typenode":false,"inputs":{"input_1":{"connections":[{"node":"2","input":"output_1"}]}},"outputs":{"output_1":{"connections":[]}},"pos_x":754,"pos_y":166},"2":{"id":2,"name":"f_junc","data":{},"class":"f_junc","typenode":false,"inputs":{"input_1":{"connections":[{"node":"4","input":"output_1"}]}},"outputs":{"output_1":{"connections":[{"node":"1","output":"input_1"},{"node":"3","output":"input_1"}]}},"pos_x":467,"pos_y":328},"3":{"id":3,"name":"e_store","data":{},"class":"e_store","typenode":false,"inputs":{"input_1":{"connections":[{"node":"2","input":"output_1"}]}},"outputs":{"output_1":{"connections":[]}},"pos_x":813,"pos_y":432},"4":{"id":4,"name":"sf","data":{},"class":"sf","typenode":false,"inputs":{},"outputs":{"output_1":{"connections":[{"node":"2","output":"input_1"}]}},"pos_x":206,"pos_y":222}}},"Other":{"data":{}}}}""" |> json
 end
 
 # Start the server
